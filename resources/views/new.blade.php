2333f90d7034a42e15e50993122646e125b4c203
Lukas Birkner Davila <lbirkner@ENGGMT-0001.Celmedia.chile>
2022-03-21 12:18:39 -0300

    first commit

---
+++ resources/views/new.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+</head>
+    
+<body>
+    <img src="img/coop.png" alt="err"/> 
+    <div class="head">
+    <p>"Nombre var"</p>
+    <p>COINCO Nro 1004 ALERCE SUR</p>
+    <p>PUERTO MONTT-REGION DE LOS LAGOS</p>
+    <br>
+    <br>
+    <br>
+    </div>
+    <h3 class="title">Comunicación Trimestral Crédito de Consumo</h3>
+    <table border="1">
+        <tbody>
+            <tr>
+                <td class="cell">Nombre Titular</td>
+                <td class="cell_info">x</td>
+            </tr>
+            <tr>
+                <td class="cell">Fecha de la información</td>
+                <td class="cell_info">x</td>
+            </tr>
+            <tr>
+                <td class="cell">ID Producto</td>
+                <td class="cell_info">x</td>
+            </tr>
+        </tbody>
+    </table>
+    <h3 class="subt">I. Producto Principal</h3>
+    <hr>
+    <div class="product">
+        <div class="product-div">
+            <table border="1" class="table1">
+                <tbody>
+                    <tr>
+                        <td class="cell">Plazo de Crédito:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Saldo del Crédito:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Valor de la cuota (pesos):</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Fecha Póximo Pago:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Costo Total de Prepago:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Garantias Vigentes:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                    <tr>
+                        <td class="cell">Número de Cuota:</td>
+                        <td class="cell">x</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="caev-div">
+            <table border="1" class="caev_table">
+                <td class="caev">CAEV :<br>18.00%</td>
+            </table>
+        </div>
+    </div>
+    <div>
+        <h3 class="subt">II. Historial</h3>
+        <hr>
+        <table border="1">
+            <tbody>
+                <tr>
+                    <td class="cell-2">Número de Cuotas Pagadas:</td>
+                    <td class="cell_info">x</td>
+                </tr>
+                <tr>
+                    <td class="cell-2">Número de Cuotas Vencidas no Pagadas:</td>
+                    <td class="cell_info">x</td>
+                </tr>
+                <tr>
+                    <td class="cell-2">Monto Vencido no Pagado</td>
+                    <td class="cell_info">x</td>
+                </tr>
+            </tbody>
+        </table>
+        <p class="spaced">Detalle</p>
+    </div>
+    <div>
+        <table border="1" class="table2">
+            <tbody>
+                <tr>
+                    <td class="cell">Cuota N°</td>
+                    <td class="cell">Monto Pagado</td>
+                    <td class="cell">Fecha vencimiento</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+                <tr>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                    <td class="cell">x</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <h3 class="subt">III. Gastos o Cargos por Productos o Servicios Voluntariamente Contratados</h3>
+    <hr>
+    <p class="spaced">Valor de la Cuota de la Referencia: x</p>
+    <div>
+        <table border="1" class="table3">
+            <tbody>
+                <tr>
+                    <td class="small-cell">Tipo Seguro</td>
+                    <td class="small-cell">Costo Mensual ($)</td>
+                    <td class="small-cell">Costo Total ($)</td>
+                    <td class="small-cell">Proveedor de Servicios</td>
+                    <td class="small-cell">Cobertura</td>
+                </tr>
+                <tr>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                </tr>
+                <tr>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                </tr>
+                <tr>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                </tr>
+                <tr>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                </tr>
+                <tr>
+                    <td class="small-cell">Total:</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                    <td class="small-cell">x</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <h3 class="subt">IV. Condiciones de Prepago</h3>
+    <hr>
+    <div class="flex spaced">
+        <div class="question">
+            <p>Cargo de Prepago: </p>
+            <p>Plazo Aviso de Prepago: </p>
+        </div>
+        <div class="answer">
+            <p>x</p>
+            <p>x</p>
+        </div>
+    </div>
+    <h3 class="subt">V. Recargo por Atrasos</h3>
+    <hr>
+    <div class="flex spaced">
+        <div class="question">
+            <p>Interes Moratorio: </p>
+            <p>Gastos de Cobranza (%): </p>
+        </div>
+        <div class="answer">
+            <p>Tasa Máxima Convencional</p>
+            <p>Un 9% para las deudas o cuotas de hasta 10 UF</p>
+            <p>Un 6% para las deudas de 10 y hasta 50 UF</p>
+            <p>Un 9% para deudas de más de 50 UF</p>
+        </div>
+    </div>
+    <p class="spaced">El costo total de prepago no considera los recargos por atrasos en el pago del crédito</p>
+    
+</body>
+</html>
